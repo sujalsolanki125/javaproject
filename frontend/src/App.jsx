3c5f273f74a592be2ade0592efbfb4b3125dec56
Sujalkumar Solanki <sujalkumarsolanki2005@gmail.com>
2025-12-24 11:59:02 +0530

    Add react-hot-toast package and Toaster component for notifications

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,14 @@
 import AppRoutes from './routes/AppRoutes';
 import './styles/index.css';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <>
+      <AppRoutes />
+      <Toaster position="top-right" />
+    </>
+  );
 }
 
 export default App;
