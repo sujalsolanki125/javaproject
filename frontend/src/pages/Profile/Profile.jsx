ec968a831e361139e577569f31bca44531dcb85d
Sujalkumar Solanki <sujalkumarsolanki2005@gmail.com>
2025-12-24 12:33:05 +0530

    Fix authService import in Profile.jsx

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import authService from '../../services/auth.service';
+import { authService } from '../../services/auth.service';
 import userService from '../../services/user.service';
 import carbonService from '../../services/carbon.service';
 
